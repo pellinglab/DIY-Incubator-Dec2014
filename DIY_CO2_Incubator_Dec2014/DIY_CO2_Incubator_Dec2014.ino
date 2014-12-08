4dca1670a15bee30cf614a67aa58c65283e439c0
Pelling Lab <a@pellinglab.net>
2014-12-08 12:04:28 +0800

    Initial commit

---
+++ DIY_CO2_Incubator_Dec2014/DIY_CO2_Incubator_Dec2014.ino
@@ -0,0 +1,197 @@
+/*
+DIY CO2 INCUBATOR FOR MAMMALIAN CELL CULUTRE (ARDUINO UNO R3)
+
+DECEMBER 2014. 
+WRITTEN BY ANDREW E. PELLING 
+http://www.pellinglab.net 
+
+Simple incubator with 12V resistive heaters, 12V normally closed solenoid and CO2 Sensor. 
+VERY simple relay control over heating and solenoid valve on CO2 tank. 
+
+CO2 Meter (SprintIR 0-20% GC-0017):
+http://www.co2meter.com/collections/co2-sensors/products/sprintir-100-percent-co2-sensor
+
+Also need the latest COZIR library, download from this thread (Using Cozir 0-1-01.zip in this code):
+http://forum.arduino.cc/index.php?topic=91467.0
+
+Temperature Sensors:
+DS18B20 Digital Temp Sensors wired parasitically.
+Useful for having more than one sensor on a single Arduino Pin.
+http://openenergymonitor.org/emon/buildingblocks/DS18B20-temperature-sensing
+
+Sensors require DallasTemperature.h Library here:
+http://milesburton.com/Dallas_Temperature_Control_Library
+
+Possible improvements could be to utilize PID library and PWM control the heaters.
+
+CO2 supplied by BOC Gas, SodaStream or Paintball CO2 tanks. 
+
+Arduino Pin Connections:
+Pin 2,3: Tx/Rx from CO2 Sensor
+Pin #5: Inidicator LED
+Pin #7: Heater Relay
+Pin #8: Solenoid Relay
+Pin #9: PWM Fan Control
+*/
+
+///////// TEMP AND CO2 SETPOINTS ///////// 
+// MUST be set according to your own system. TRIAL AND ERROR.
+// May need to increase/decrease depending on size/shape of incubator, CO2 flow rate, etc.
+// Ideal is 37.0 and 5.0 (Temp and CO2).
+float TSetpoint = 36.9;
+float CO2Setpoint = 4.8;
+///////// TEMP AND CO2 SETPOINTS ///////// 
+
+////////* LIBRARIES  *////////
+// DS18B20 Temp Sensors
+#include "OneWire.h"
+#include "DallasTemperature.h"
+
+// CO2 Sensors
+#include "SoftwareSerial.h"
+#include "cozir.h"
+
+// Adafruit 7-segment Display for updating CO2 and Temperature
+#include "Wire.h"
+#include "Adafruit_LEDBackpack.h"
+#include "Adafruit_GFX.h"
+////////* LIBRARIES  *////////
+
+////////* RELAY and FAN CONTROL *////////
+int HeatingIndicatorLED = 5; // indicator LED for heaters
+int Heater = 7;  // Relay for controlling power to heaters
+int Solenoid = 8; // Relay for controlling 12V solenoid valve
+int fan = 9; // PWM control over 12V fan
+////////* RELAY and FAN CONTROL *////////
+
+////////* ONE WIRE DS18B20 TEMP SENSORS  *////////
+#define ONE_WIRE_BUS 6 // Sensors on digital pin 4 of the Arduino
+OneWire oneWire(ONE_WIRE_BUS); // Setup a oneWire instance to communicate with ANY OneWire devices
+DallasTemperature sensors(&oneWire); // Pass our oneWire reference to Dallas Temperature.
+float T1, T2, AvgT, SingleT  = 0;
+////////* ONE WIRE DS18B20 TEMP SENSORS  *////////
+
+////////* SPRINTIR COZIR CO2 SENSOR  *////////
+SoftwareSerial nss(2,3); // Rx,Tx - Pin 5,7 on sensor
+COZIR czr(nss);
+float SingleCO2, CO2 = 0;
+float multiplier = 0.001;  // 10/10000 (Hardware multiplier/ppm conversion).  
+float reading = 0;
+////////* SPRINTIR COZIR CO2 SENSOR  *////////
+
+///////// MATRIX LED DISPLAY SETUP ///////// 
+Adafruit_7segment matrix = Adafruit_7segment();
+long previousMillis = 0;        // will store last time LED was updated
+int update = 0;
+///////// LED DISPLAY SETUP /////////
+
+void setup()
+{
+  Serial.begin(9600);   // Start serial port
+  //czr.SetOperatingMode(CZR_POLLING);  // Start the CO2 sensor and put into POLLING mode
+  sensors.begin();      // Start up the temperature sensor library
+  matrix.begin(0x70);   // Start Matrix
+  matrix.print(8888);   // Print '8888' on matrix
+  matrix.writeDisplay();
+  pinMode(fan, OUTPUT);          // Set pin for output to control TIP120 Base pin
+  pinMode(Heater, OUTPUT);       // Sets pin for controlling heater relay
+  digitalWrite(Heater, LOW);     // Set LOW (heater off)
+  pinMode(Solenoid, OUTPUT);     // Sets pin for controlling solenoid relay
+  digitalWrite(Solenoid, LOW);   // Set LOW (solenoid closed off)
+  pinMode(HeatingIndicatorLED, OUTPUT);    // LED for indicating heating is on
+  digitalWrite(HeatingIndicatorLED, LOW);  // Turn LED off
+  analogWrite(fan, 255);  // Turn on the Fan
+  delay(2000);  // Wait
+}
+
+void loop()
+{
+  // Read the average temp between 2 sensors 3 times and find the average.
+  for (int i = 0; i<3; i++) {
+    sensors.requestTemperatures(); // Send the command to get temperatures
+    T1=sensors.getTempCByIndex(0);
+    T2=sensors.getTempCByIndex(1);
+    SingleT += (T1+T2)/2;
+  }
+  
+  AvgT = SingleT/3;
+  SingleT = 0;
+  
+  // Turn on/off heater based on temperature reading
+  if (AvgT < TSetpoint) {
+    digitalWrite(Heater, HIGH);    
+    analogWrite(HeatingIndicatorLED, 255);
+  } else if (AvgT > TSetpoint) {
+    digitalWrite(Heater, LOW);
+    analogWrite(HeatingIndicatorLED, 0);
+  } 
+
+  // Read CO2 sensor 3 times and find determine the average.
+  for (int i = 0; i<3; i++) {
+    SingleCO2 += czr.CO2()*multiplier;
+  }
+  CO2 = SingleCO2/3;
+  SingleCO2 = 0;
+  
+  CO2 = czr.CO2()*multiplier;
+  
+  // Open/close the solenoid valve based on the current CO2 reading 
+  // Solenoid  opens for 0.2sec, followed by a 2sec delay to mix air/CO2
+  if (CO2 < CO2Setpoint) {
+    digitalWrite(Solenoid, HIGH);
+    delay(200);
+    digitalWrite(Solenoid, LOW);
+    delay(500);
+  } else if (CO2 > CO2Setpoint) {
+    digitalWrite(Solenoid, LOW);
+  } 
+
+  updateMatrix(); // Update the Matrix display, 
+  
+  // SerialPrintResults(); // UNCOMMENT to print results over Serial port for debugging
+}
+
+void updateMatrix() {
+  unsigned long currentMillis = millis();
+  
+  // Alternate the display between Temperature and CO2 every 2 seconds
+  if (currentMillis - previousMillis > 2000) {
+    previousMillis = currentMillis;
+    if (update == 0) {
+      matrix.print(AvgT, 1);
+      matrix.writeDisplay();
+      update = 1;
+    } else if (update == 1) {
+      matrix.print(CO2, 1);
+      matrix.writeDisplay();
+      update = 0;
+    }
+  }
+}
+
+// For debugging only, uncomment line 151
+void SerialPrintResults()
+{
+  Serial.println("-----------------------------------------------");
+  Serial.println();
+  Serial.print("Temperature for Device 0 is: ");
+  Serial.print(T1);
+  Serial.println("*C");
+  Serial.print("Temperature for Device 1 is: ");
+  Serial.print(T2);
+  Serial.println("*C");
+  Serial.println();
+  Serial.print("Average Temperature is: ");
+  Serial.print(AvgT);
+  Serial.println("*C");
+  Serial.println();
+  Serial.print("CO2 Content is: ");
+  Serial.print(CO2);
+  Serial.println("%");
+  Serial.println();
+  Serial.println("-----------------------------------------------");
+  Serial.println();  
+}
+
+
+
